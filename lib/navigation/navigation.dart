37d769a931527fef1ef874b061b2f6f8851e6d7f
Aniket <aniket_ch05@outlook.com>
2021-12-23 10:19:27 +0530

    google login initial commit

---
+++ lib/navigation/navigation.dart
@@ -0,0 +1,15 @@
+import 'package:companion/screens/home_page.dart';
+import 'package:companion/screens/sign_in_page.dart';
+import 'package:companion/screens/welcome_page.dart';
+import 'package:flutter/material.dart';
+
+
+
+
+class Navigate {
+  static Map<String, Widget Function(BuildContext)> routes =   {
+    '/' : (context) => WelcomePage(),
+    '/sign-in' : (context) => SignInPage(),
+    '/home'  : (context) => HomePage()
+  };
+}
